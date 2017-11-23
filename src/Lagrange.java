e550186feeb480b057fed09594a1e323df09f4ba
Khalil <khalil.va@gmail.com>
2017-11-23 15:20:05 -0500

    factorization

--- src/Lagrange.java
+++ src/Lagrange.java
@@ -1,32 +1,51 @@
 
-import java.util.Vector;
 
 public class Lagrange  {
-	private int n = 0;
-	private double sum = 0, product = 0;
+	
 
 	public static void main (String[] args){
 		int[] xx = new int[]{11, 22, 33, 44, 55, 66, 77, 88, 99, 111};
 		int[] yy = new int[]{42263, 7819, 64503, 59707, 30654, 7787, 14769, 2946, 47958, 7983};
 		
-		System.out.println((new Lagrange()).calculateResult(5, xx, yy));
+		int[] i = new int[]{0, 8, 1, 3, 5};
+		
+		int[] xx2 = new int[]{xx[i[0]], xx[i[1]], xx[i[2]], xx[i[3]], xx[i[4]]};
+		int[] yy2 = new int[]{yy[i[0]], yy[i[1]], yy[i[2]], yy[i[3]], yy[i[4]]};
+		
+		int[] xx3 = new int[]{2, 4, 5};
+		int[] yy3 = new int[]{1942, 3402, 4414};
+		
+		(new Lagrange()).calculateResult(xx, yy);
 		
 	}
 	
-	public double calculateResult(int[] xx, int[] yy){
-		n = xx.length;
-			
+	public void calculateResult(int[] xx, int[] yy){
+		int n = xx.length;
+		
+		int total = 0;
+		
 		for (int i = 0; i < n; i++) {
-			product = yy[i];
+			System.out.println("Calculating Legrange_" + i + " ");
+			double leg = 1;
+			
 			for (int j = 0; j <  n; j++) {
+				
 				if (i != j) {
-					product = product * (0 - xx[j]) / (xx[i] - xx[j]);
+					double ln = (0 - xx[j]);
+					double ld = (xx[i] - xx[j]);
+					System.out.print(" 0-" + xx[j] + "/" + xx[i] +"-"+ xx[j] + "["+ln/ld+"]");
+					leg = leg * ln/ld;
+					if (j != n-1) System.out.print(" x");
 				}
+				
+				
 			}
-			sum = sum + product;
+			System.out.println("\n"+leg + " x " + yy[i]);
+			total += leg * yy[i];
+			System.out.println();
 		}
 		
-		return sum;
+		System.out.println("FINAL: " + total % 65537);
 	}
 
 	@Override
---
+++ src/PrimeFactorization.java
@@ -0,0 +1,43 @@
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+    
+
+public class PrimeFactorization {
+    private final static BigInteger ZERO = new BigInteger("0");
+    private final static BigInteger ONE  = new BigInteger("1");
+    private final static BigInteger TWO  = new BigInteger("2");
+    private final static SecureRandom random = new SecureRandom();
+
+    public static BigInteger prho(BigInteger product) {
+        BigInteger divisor;
+        
+        BigInteger a  = new BigInteger(product.bitLength(), random);
+        BigInteger b  = new BigInteger(product.bitLength(), random);
+        BigInteger c = b;
+
+        if (product.mod(TWO).compareTo(ZERO) == 0) return TWO;
+
+        do {
+            b  =  b.multiply(b).mod(product).add(a).mod(product);
+            c = c.multiply(c).mod(product).add(a).mod(product);
+            c = c.multiply(c).mod(product).add(a).mod(product);
+            divisor = b.subtract(c).gcd(product);
+        } while((divisor.compareTo(ONE)) == 0);
+
+        return divisor;
+    }
+
+    public static void factor(BigInteger N) {
+        if (N.compareTo(ONE) == 0) return;
+        BigInteger divisor = prho(N);
+        factor(divisor);
+        factor(N.divide(divisor));
+    }
+
+ 
+    public static void main(String[] args) {
+        BigInteger N = new BigInteger("13029506445953503759481");
+        factor(N);
+    }
+}
