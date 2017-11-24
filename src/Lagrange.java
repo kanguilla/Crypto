c71a5541809f29a2dc586a75bca0175813c2ab80
Khalil <khalil.va@gmail.com>
2017-11-24 18:52:39 -0500

    last

--- src/Lagrange.java
+++ src/Lagrange.java
@@ -1,55 +1,93 @@
-
+import java.math.BigInteger;
 
 public class Lagrange  {
 	
 
 	public static void main (String[] args){
-		int[] xx = new int[]{11, 22, 33, 44, 55, 66, 77, 88, 99, 111};
-		int[] yy = new int[]{42263, 7819, 64503, 59707, 30654, 7787, 14769, 2946, 47958, 7983};
+		int[] x = new int[]{15101, 11, 22, 33, 44, 55, 66, 77, 88, 99, 111};
+		int[] y = new int[]{3693, 42263, 7819, 64503, 59707, 30654, 7787, 14769, 2946, 47958, 7983};
 		
-		int[] i = new int[]{0, 8, 1, 3, 5};
+		int[] i = new int[]{0, 1, 2, 3, 4};
+
+		int[] x2 = new int[]{x[i[0]], x[i[1]], x[i[2]], x[i[3]], x[i[4]]};
+		int[] y2 = new int[]{y[i[0]], y[i[1]], y[i[2]], y[i[3]], y[i[4]]};
+
+		int[] x4 = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+		int[] y4 = new int[]{16, 5, 5, 16, 7, 9, 22, 15};
 		
-		int[] xx2 = new int[]{xx[i[0]], xx[i[1]], xx[i[2]], xx[i[3]], xx[i[4]]};
-		int[] yy2 = new int[]{yy[i[0]], yy[i[1]], yy[i[2]], yy[i[3]], yy[i[4]]};
-		
-		int[] xx3 = new int[]{2, 4, 5};
-		int[] yy3 = new int[]{1942, 3402, 4414};
-		
-		(new Lagrange()).calculateResult(xx, yy);
-		
-	}
-	
-	public void calculateResult(int[] xx, int[] yy){
-		int n = xx.length;
-		
-		int total = 0;
-		
-		for (int i = 0; i < n; i++) {
-			System.out.println("Calculating Legrange_" + i + " ");
-			double leg = 1;
-			
-			for (int j = 0; j <  n; j++) {
+		int[] x3 = new int[]{1, 5, 7};
+		int[] y3 = new int[]{16, 7, 22};
 				
-				if (i != j) {
-					double ln = (0 - xx[j]);
-					double ld = (xx[i] - xx[j]);
-					System.out.print(" 0-" + xx[j] + "/" + xx[i] +"-"+ xx[j] + "["+ln/ld+"]");
-					leg = leg * ln/ld;
-					if (j != n-1) System.out.print(" x");
-				}
-				
-				
-			}
-			System.out.println("\n"+leg + " x " + yy[i]);
-			total += leg * yy[i];
-			System.out.println();
+		
+		
+		
+		
+		
+		
+		//System.out.println(findSecret3(x, y, 65537));
+		//System.out.println(findSecret3(x2, y2, 65537));
+		//System.out.println(findSecret3(x3, y3, 31));
+		
+		BigInteger[] x1 = new BigInteger[5];
+		BigInteger[] y1 = new BigInteger[5];
+		
+		for (int k = 0; k<x1.length;k++) {
+			x1[k] = BigInteger.valueOf(x[i[k]]);
+			y1[k] = BigInteger.valueOf(y[i[k]]);
 		}
 		
-		System.out.println("FINAL: " + total % 65537);
+		
+		try {
+			System.out.println(findSecret(x1, y1, BigInteger.valueOf(65537)).intValue());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+//		int a = 0;
+//		while (a != 51515) {
+//			x1[0] = x1[0].add(BigInteger.ONE);
+//			System.out.println(x1[0]);
+//			try {
+//				a = findSecret(x1, y1, BigInteger.valueOf(65537)).intValue();
+//			}catch(Exception e) {
+//				continue;
+//			}
+//		}
+//		try {
+//			System.out.println("the number is " + x1[0]);
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 
-	@Override
-	public String toString(){
-		return "Lagrange";
+	
+	public static BigInteger findSecret(BigInteger[] x, BigInteger[] y, BigInteger p) throws Exception{
+
+		//System.out.print("Legrange Interpolation using values under group "+p+": ");
+		for (int n = 0; n < x.length; n++) {
+			//System.out.print("(" + x[n] + "," + y[n] + ") ");
+		}
+		//System.out.println("\n");
+		BigInteger s = BigInteger.ZERO;		
+		for (int i = 0; i < x.length; i++) {
+			//System.out.println("Calculating Legrange_" + i + " ");
+			BigInteger c = y[i];
+			for (int j = 0; j < x.length; j++) {
+				if (j == i) continue;
+				BigInteger ln = (x[j]);
+				BigInteger ld = (x[i].subtract(x[j])).mod(p);
+				
+				BigInteger temp = ld.modInverse(p);
+				//System.out.print(x[j] + "/" + x[i] +"-"+ x[j]);
+				c = ln.multiply(temp).mod(p).multiply(c).mod(p);
+				//if (j != x.length-1) System.out.print(" x ");
+			}
+			s = s.add(c.mod(p));
+			//System.out.println();
+		}
+		//System.out.println("Secret: " + s.mod(p));
+		return s.mod(p);
 	}
+	
 }
